a3b68b8e3db9fbad67cfa7df8608e97903561ae8
Vasyl Airiian <39553581+Reshayri@users.noreply.github.com>
2022-11-30 18:01:50 +0100

    Add files via upload

---
+++ HOBODY/NOBODY4/denobody.js
@@ -0,0 +1,192 @@
+function de1() {
+    var message;
+    message =
+        '1. Niemand stopft Socken. Und wenn Sie tiefer in die Geschichte eintauchen: Keiner der unter Sechzigjährigen weiß, was es heißt, einen Anzug oder Mantel umzudrehen. ';
+    document.getElementById("de1").innerHTML = message;
+
+    //alert(message);
+}
+function de2() {
+    var message;
+    message= '2. Niemand sonst putzt Teppiche mit dem ersten Schnee oder Sauerkrautsaft.';
+    document.getElementById("de2").innerHTML= message;
+}
+function de3() {
+    var message;
+    message= '3. Niemand wischt mehr - Dreifach-Köln-Tonabnehmer in einem Kassettenrecorder. Da sie den gekauten Film nicht in Kassetten mit Lack kleben.';
+    document.getElementById("de3").innerHTML= message;
+}
+function de4() {
+    var message;
+    message= '4. Niemand schneidet mehr Fernsehsendungen aus der Samstagszeitung und hebt darin keine interessanten Sendungen hervor, für die Sie rechtzeitig erscheinen müssen.';
+    document.getElementById("de4").innerHTML= message;
+}
+function de5() {
+    var message;
+    message= '5. Niemand schon versendet Serverat nicht in Paketen.';
+    document.getElementById("de5").innerHTML= message;
+}
+function de6() {
+    var message;
+    message= '6. Niemand bewahrt schon leere Bierdosen in einer Anrichte auf.';
+    document.getElementById("de6").innerHTML= message;
+}
+function de7() {
+    var message;
+    message= '7. Niemand sonst rühmt sich der Fähigkeit, ein Streichholz anzuzünden, indem er es an einer Fensterscheibe oder einem Hosenbein anzündet.';
+    document.getElementById("de7").innerHTML= message;
+}
+function de8() {
+    var message;
+    message= '8. Niemand sonst glaubt, dass das beste Hustenmittel nachts Dosen oder ein Honigwickel sind.';
+    document.getElementById("de8").innerHTML= message;
+}
+function de9() {
+    var message;
+    message= '9.Niemand hängt mehr ein Sieb an den Ausguss einer Teekanne.';
+    document.getElementById("de9").innerHTML= message;
+}
+function de10() {
+    var message;
+    message= '10. Niemand macht mehr die Show eine Decke in einem Bettbezug durch ein Loch in der Mitte.';
+    document.getElementById("de10").innerHTML= message;
+}
+// -------------------------------------------1-------------------------10-------------------------
+function de11() {
+    var message;
+    message= '11. Niemand wäscht schon keine Plastiktüten.';
+    document.getElementById("de11").innerHTML= message;
+}
+function de12() {
+    var message;
+    message= '12. Niemand startet schon jeden Abend die Uhr und den Wecker.';
+    document.getElementById("de12").innerHTML= message;
+}
+function de13() {
+    var message;
+    message= '13. Niemand sonst spritzt beim Hosenbügeln Wasser aus dem Mund.';
+    document.getElementById("de13").innerHTML= message;
+}
+function de14() {
+    var message;
+    message= '14. Lange Zeit niemand Putzen Sie Ihre Zähne nicht mit einer Naturborsten-Zahnbürste. Seltsam, aber sie waren die billigsten.';
+    document.getElementById("de14").innerHTML= message;
+}
+function de15() {
+    var message;
+    message= '15. Niemand serviert mehr gekaufte Knödel als Hauptgericht auf der festlich gedeckten Tafel.';
+    document.getElementById("de15").innerHTML= message;
+}
+function de16() {
+    var message;
+    message= '16. Niemand sonst wickelt Watte um ein Streichholz, um sich die Ohren zu reinigen.';
+    document.getElementById("de16").innerHTML= message;
+}
+function de17() {
+    var message;
+    message= '17. Niemand erinnert sich mehr, was der Unterschied zwischen einem blauen und einem roten Radiergummi ist ... Aber ich erinnere mich! Der Blaue löscht den Bleistift und der Rote löscht die Tinte und bohrt Löcher in das Papier.';
+    document.getElementById("de17").innerHTML= message;
+}
+function de18() {
+    var message;
+    message= '18. Niemand denkt mehr daran, dass der Badetag einmal in der Woche sein sollte ...';
+    document.getElementById("de18").innerHTML= message;
+}
+function de19() {
+    var message;
+    message= '19. Niemand sonst sammelt hilfreiche Tipps aus Abreißkalendern.';
+    document.getElementById("de19").innerHTML= message;
+}
+function de20() {
+    var message;
+    message= '20. Niemand sonst klebt Übersetzer auf eine Kachel.';
+    document.getElementById("de20").innerHTML= message;
+}
+//---------------------------------------------------------------11---------------------------------20----------------------
+function de21() {
+    var message;
+    message= '21. Niemand geht schon in ein Fotostudio, um ein jährliches Familienporträt zu machen.';
+    document.getElementById("de21").innerHTML= message;
+}
+function de22() {
+    var message;
+    message= '22. Niemand schmückt mehr die Wände der Wohnung durch Einbrennen oder Prägen aus eigener Herstellung.';
+    document.getElementById("de22").innerHTML= message;
+}
+function de23() {
+    var message;
+    message= '23. Niemand strickt schon Schleifen am Gitarrenhals';
+    document.getElementById("de23").innerHTML= message;
+}
+function de24() {
+    var message;
+    message= '24. Niemand wird sich daran erinnern, wenn ein T-Shirt unter einem Pullover hervorschaut - das nennt man "von unter Freitag Samstag" und im Allgemeinen ist es einfach unanständig!';
+    document.getElementById("de24").innerHTML= message;
+}
+function de25() {
+    var message;
+    message= '25. Niemand sonst Lassen Sie das Öl für das "nächste Mal" in der Pfanne.';
+    document.getElementById("de25").innerHTML= message;
+}
+function de26() {
+    var message;
+    message= '26. Niemand hat mehr Angst, dass einem der Abwassertank eines Tages doch noch auf den Kopf fällt.';
+    document.getElementById("de26").innerHTML= message;
+}
+function de27() {
+    var message;
+    message= '27. Und seit langem habe ich keine neuen Witze über Stirlitz und Wassili Iwanowitsch gehört.';
+    document.getElementById("de27").innerHTML= message;
+}
+function de28() {
+    var message;
+    message= '28. Traurig... 😥 Wir werden die erste Generation sein, die keine Spuren hinterlässt.';
+    document.getElementById("de28").innerHTML= message;
+}
+function de29() {
+    var message;
+    message= '29. Wir werden unsere Briefe nicht hinterlassen. Von mir selbst aus meiner Jugend, wenn es so scharf und untrennbar ist, und später merkt man, dass es nicht damit zusammenhängt. Wir werden keine Briefe von uns selbst hinterlassen, für Freunde, mit denen wir uns nach unwichtigen Tagen vorher sehnen. Wir werden unsere Handschrift, abgenutztes, zerknittertes, gefaltetes Papier, Umschläge mit Adressen, Briefmarken, Namen von wem und von wem nicht hinterlassen.';
+    document.getElementById("de29").innerHTML= message;
+}
+function de30() {
+    var message;
+    message= '30.Wir hinterlassen keine Fotos. Sie werden alle in der elektronischen Aufregung zugrunde gehen. Schon jetzt können wir unser Fotoalbum nicht herausnehmen, wir können schon jetzt nicht auf die Rückseite schreiben - es gibt keine Wendungen. Ich kann nirgendwo schreiben, dass ich es bin, und das ist Zhenya, das ist Masha, das sind wir. Wir werden unsere Gesichter nicht verlassen.';
+    document.getElementById("de30").innerHTML= message;
+}
+//--------------------------------------------------------21-30-------------------------------------------------------------------------------
+function de31() {
+    var message;
+    message= '31. Werden keine Memoiren, Tagebücher, Notizen, Briefe, Fotografien verschiedener Jahre sein. Es wird keine Biografien geben, keine Handschriften, alles, was für jemanden ein roter Faden für uns bleibt, der für immer gegangen ist. Wir werden die Ersten sein, die sich spurlos auflösen.';
+    document.getElementById("de31").innerHTML= message;
+}
+function de32() {
+    var message;
+    message= '32. Hallo, Ära der Gadgets, Computer und sozialen Netzwerke. Du hast es geschafft, uns auszulöschen...';
+    document.getElementById("de32").innerHTML= message;
+}
+function de33() {
+    var message;
+    message= '33. Eine Großmutter 👵 sagte zu jemandem in zwei: "Weißt du, Glück ... es kann anders sein ..." Ich dachte ... na, was für ein Unsinn ... Glück ... es ist Glück - es ist, wenn alles ist perfekt - gut ... absolut alles.';
+    document.getElementById("de33").innerHTML= message;
+}
+function de34() {
+    var message;
+    message= '34. Und erst jetzt ... nach Jahren ... fange ich an zu verstehen ... Glück ist nicht absolut ... es ist relativ ...';
+    document.getElementById("de34").innerHTML= message;
+}
+function de35() {
+    var message;
+    message= '35. Glück ... wenn es nicht wehtut ... wenn Kinder gesund sind ... wenn du jemanden hast, dem du deinen Traum erzählen kannst ... wenn du jemanden hast, an den du alleine denken kannst ... wenn es jemanden gibt, an den du denken kannst ... ... wenn es ein Haus gibt ... in das Sie zurückkehren möchten ... wenn Sie Seelenfrieden haben ... wenn Ihre Kinder zu würdigen Menschen heranwachsen ... wenn Sie Morgen haben ... wenn sie Sie einfach rufen ... es ist nur die Abwesenheit von Unglück ... es gibt tatsächlich so viel davon ... und wir warten alle darauf und warten ... ohne zu wissen ... dass es anders sein kann.';
+    document.getElementById("de35").innerHTML= message;
+}
+function de36() {
+    var message;
+    message= '36. Diese Liste von Erinnerungen geht weiter und weiter! Aber ich werde nicht. Lass den Leser machen.';
+    document.getElementById("de36").innerHTML= message;
+}
+function de37() {
+    var message;
+    message= '37. Von mir selbst werde ich hinzufügen. Zum Beispiel kostet 1 kW Strom 4 Kopeken. Eine Flasche Wodka wurde in einem Geschäft für 3 Rubel 62 Kopeken gekauft. Es gab so eine Anekdote: Ein Ehemann von unten ruft seiner Frau zu, die im 6. Stock ist: - Lucy, wirf mir 62 Kopeken zu! - Wie wird der Wurf auseinanderfallen! - Und Sie wickeln es in einen Drei-Rubel-Schein! Es gibt noch eine andere Anekdote über eine Radioröhre wie: 3P62K ...';
+    document.getElementById("de37").innerHTML= message;
+}
+//---------------------------------------------------------------------31-37-----------------------------------------------------------------
---
+++ HOBODY/NOBODY4/nobody2.js
@@ -0,0 +1,188 @@
+function trans1() {
+    var message;
+    message =
+        '1. Nobody darns socks. And if you go deeper into history - none of those who are under sixty know what it is to turn a suit or coat over. ';
+    document.getElementById("demo1").innerHTML = message;
+
+    //alert(message);
+}
+function trans2() {
+    var message;
+    message= '2. Nobody else  cleans carpets with the first snow or juice from sauerkraut.';
+    document.getElementById("demo2").innerHTML= message;
+}
+function trans3() {
+    var message;
+    message= '3. No one wipes anymore  triple cologne pickup head in a cassette recorder. As they do not glue the chewed film in cassettes with varnish.';
+    document.getElementById("demo3").innerHTML= message;
+}
+function trans4() {
+    var message;
+    message= '4. Nobody cuts anymore TV programs from the Saturday newspaper and does not emphasize interesting programs in it that you need to be in time for.';
+    document.getElementById("demo4").innerHTML= message;
+}
+function trans5() {
+    var message;
+    message= '5. Nobody already does not send serverat in parcels.';
+    document.getElementById("demo5").innerHTML= message;
+}
+function trans6() {
+    var message;
+    message= '6. Nobody already  do not keep empty beer cans in a sideboard.';
+    document.getElementById("demo6").innerHTML= message;
+}
+function trans7() {
+    var message;
+    message= '7. Nobody else boasts of the ability to light a match by striking it on a window pane or on a pant leg.';
+    document.getElementById("demo7").innerHTML= message;
+}
+function trans8() {
+    var message;
+    message= '8. Nobody else believes that the best cough remedy is cans or a honey compress at night.';
+    document.getElementById("demo8").innerHTML= message;
+}
+function trans9() {
+    var message;
+    message= '9. No one hangs a strainer on the spout of a teapot anymore.';
+    document.getElementById("demo9").innerHTML= message;
+}
+function trans10() {
+    var message;
+    message= '10. Nobody runs the show anymore a blanket in a duvet cover through a hole in the middle.';
+    document.getElementById("demo10").innerHTML= message;
+}
+function trans11() {
+    var message;
+    message= '11. Nobody already does not wash plastic bags.';
+    document.getElementById("demo11").innerHTML= message;
+}
+function trans12() {
+    var message;
+    message= '12. Nobody already every evening does not start the clock and the alarm clock.';
+    document.getElementById("demo12").innerHTML= message;
+}
+function trans13() {
+    var message;
+    message= '13. Nobody else splashes water from his mouth while ironing trousers.';
+    document.getElementById("demo13").innerHTML= message;
+}
+function trans14() {
+    var message;
+    message= '14. Nobody for a long time Do not brush your teeth with a natural bristle toothbrush. Strange, but they were the cheapest.';
+    document.getElementById("demo14").innerHTML= message;
+}
+function trans15() {
+    var message;
+    message= '15. Nobody serves anymore purchased dumplings, as the main dish on the festive table.';
+    document.getElementById("demo15").innerHTML= message;
+}
+function trans16() {
+    var message;
+    message= '16. Nobody else  wraps cotton wool around a match to clean his ears.';
+    document.getElementById("demo16").innerHTML= message;
+}
+function trans17() {
+    var message;
+    message= '17. Nobody remembers anymore what is the difference between a blue eraser and a red one ... But I remember! The blue one erases the pencil, and the red one erases the ink and makes holes in the paper.';
+    document.getElementById("demo17").innerHTML= message;
+}
+function trans18() {
+    var message;
+    message= '18. Nobody thinks anymore that the bath day should be once a week ...';
+    document.getElementById("demo18").innerHTML= message;
+}
+function trans19() {
+    var message;
+    message= '19. Nobody else collects helpful tips from tear-off calendars.';
+    document.getElementById("demo19").innerHTML= message;
+}
+function trans20() {
+    var message;
+    message= '20. Nobody else  sticks translators on a tile.';
+    document.getElementById("demo20").innerHTML= message;
+}
+function trans21() {
+    var message;
+    message= '21. Nobody already does not go to a photo studio to take an annual family portrait.';
+    document.getElementById("demo21").innerHTML= message;
+}
+function trans22() {
+    var message;
+    message= '22. Nobody decorates anymore the walls of the apartment by burning or embossing of their own making.';
+    document.getElementById("demo22").innerHTML= message;
+}
+function trans23() {
+    var message;
+    message= '23. Nobody already  does not knit bows on the neck of the guitar';
+    document.getElementById("demo23").innerHTML= message;
+}
+function trans24() {
+    var message;
+    message= '24. Nobody will remember that when a T-shirt sticks out from under a sweater - this is called "from under Friday Saturday" and in general it is simply indecent!';
+    document.getElementById("demo24").innerHTML= message;
+}
+function trans25() {
+    var message;
+    message= '25. Nobody else Leave the oil in the pan for "next time".';
+    document.getElementById("demo25").innerHTML= message;
+}
+function trans26() {
+    var message;
+    message= '26. Nobody is afraid anymore that the drain tank will one day still fall on your head.';
+    document.getElementById("demo26").innerHTML= message;
+}
+function trans27() {
+    var message;
+    message= '27. And no one for a long time I have not heard fresh jokes about Stirlitz and Vasily Ivanovich.';
+    document.getElementById("demo27").innerHTML= message;
+}
+function trans28() {
+    var message;
+    message= '28. Sad... 😥 We will be the first generation to leave no trace.';
+    document.getElementById("demo28").innerHTML= message;
+}
+function trans29() {
+    var message;
+    message= '29. We will not leave our letters. From myself from my youth, when it is so sharp and inseparable, and later you realize that it’s not with that. We will not leave letters from ourselves older, for friends with whom we yearn for unimportant days before. We will not leave our handwriting, worn, wrinkled, folded paper, envelopes with addresses, stamps, names of those to whom and from whom.';
+    document.getElementById("demo29").innerHTML= message;
+}
+function trans30() {
+    var message;
+    message= '30.We will not leave photos. They will all perish in the electronic fuss. Even now we can’t take out our photo album, we can’t write on the back even now - there are no turns. I have nowhere to write that it is me, and this is Zhenya, this is Masha, this is us. We will not leave our faces.';
+    document.getElementById("demo30").innerHTML= message;
+}
+function trans31() {
+    var message;
+    message= '31. Will not be memoirs, diaries, notes, letters, photographs of different years. There will be no biographies, handwriting, everything that for someone remains a thread to us who have gone forever. We will be the first to dissolve without a trace.';
+    document.getElementById("demo31").innerHTML= message;
+}
+function trans32() {
+    var message;
+    message= '32. Hello,  era of gadgets, computers and social networks. You managed to erase us...';
+    document.getElementById("demo32").innerHTML= message;
+}
+function trans33() {
+    var message;
+    message= '33. One grandmother 👵 said to someone in two: "You know, happiness ... it can be different ..." I thought ... well, what nonsense ... happiness ... it is happiness - it is when everything is perfect - good ... absolutely everything.';
+    document.getElementById("demo33").innerHTML= message;
+}
+function trans34() {
+    var message;
+    message= '34. And only now... after years...  I am starting to understand... Happiness is not absolute... It is relatively...';
+    document.getElementById("demo34").innerHTML= message;
+}
+function trans35() {
+    var message;
+    message= '35. Happiness.. when it does not hurt... when kids are healthy... when you have someone to tell your dream... when you have someone to think about alone... when there is someone to think about you... when there is a house .. to which you want to return ... when you have peace of mind... when your children grow up to be worthy people... when you have morning... when they just call you... it is just the absence of unhappiness... there is actually so much of it .. and we are all waiting for it and waiting .. not knowing .. that it can be different.';
+    document.getElementById("demo35").innerHTML= message;
+}
+function trans36() {
+    var message;
+    message= '36. This list of memories goes on and on! But, I will not. Let the reader do it.';
+    document.getElementById("demo36").innerHTML= message;
+}
+function trans37() {
+    var message;
+    message= '37. From myself I will add For example, 1 kW of electricity cost 4 kopecks.   A bottle of vodka was bought in a store for 3 rubles 62 kopecks. There was such an anecdote: a husband from below shouts to his wife, who is on the 6th floor:  - Lucy, throw me 62 kopecks! - How will the throw fall apart! - And you wrap it in a three-ruble note! There is another anecdote about a radio tube like: 3P62K ...';
+    document.getElementById("demo37").innerHTML= message;
+}
